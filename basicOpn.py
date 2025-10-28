ecf11b1b81000c6f46e1c4756dbe3de6ae34be86
Harshita.Hegde <01fe24bca232@kle.ac.in>
2025-10-28 17:45:12 +0530

    addition and subtraction

---
+++ basicOpn.py
@@ -0,0 +1,11 @@
+# Input two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Addition and subtraction
+sum_result = a + b
+diff_result = a - b
+
+# Display results
+print("Sum:", sum_result)
+print("Difference:", diff_result)
